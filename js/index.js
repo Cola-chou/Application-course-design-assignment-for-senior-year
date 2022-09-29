7a8668f629913f19d412d4604aa84c7554e43405
Cola-chou <1546098718@qq.com>
2022-09-29 11:04:35 +0800

    最后两步

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+//bar chart
 (function () {
   var chartDom = document.querySelector('.bar .chart');
   var myChart = echarts.init(chartDom);
@@ -317,149 +318,250 @@
   })
   option && myChart.setOption(option);
 })();
+// line2 chart
 (function () {
-
   var chartDom = document.querySelector('.line2 .chart')
   var myChart = echarts.init(chartDom);
   var option;
-
   option = {
-    xAxis: {
-      type: 'category',
-      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    tooltip: {
+      trigger: "axis",
+      axisPointer: {
+        lineStyle: {
+          color: "#dddc6b"
+        }
+      }
     },
-    yAxis: {
-      type: 'value'
+
+    legend: {
+      top: "0%",
+      textStyle: {
+        color: "rgba(255,255,255,.5)",
+        fontSize: "12"
+      }
     },
+
+    grid: {
+      left: "10",
+      top: "30",
+      right: "10",
+      bottom: "10",
+      containLabel: true
+
+    },
+
+    xAxis: [
+      {
+        type: "category",
+        boundaryGap: false,
+        axisLabel: {
+          textStyle: {
+            color: "rgba(255,255,255,.6)",
+            fontSize: 12
+          }
+        },
+        axisLine: {
+          lineStyle: {
+            color: "rgba(255,255,255,.2)"
+          }
+        },
+        data: [
+          "01", "02", "03", "04", "05", "06", "07", "08", "09",
+          "11", "12", "13", "14", "15", "16", "17", "18", "19",
+          "20", "21", "22", "23", "24", "25", "26", "27", "28",
+          "29", "30"
+
+        ]
+      },
+      {
+        axisPointer: { show: false },
+        axisLine: { show: false },
+        position: "bottom",
+        offset: 20
+      }
+    ],
+    yAxis: [
+      {
+        type: "value",
+        axisTick: { show: false },
+        axisLine: {
+          lineStyle: {
+            color: "rgba(255,255,255,.1)"
+          }
+        },
+        axisLabel: {
+          textStyle: {
+            color: "rgba(255,255,255,.6)",
+            fontSize: 12
+          }
+        },
+        splitLine: {
+          lineStyle: {
+            color: "rgba(255,255,255,.1)"
+          }
+        }
+      }
+    ],
     series: [
       {
-        data: [150, 230, 224, 218, 135, 147, 260],
-        type: 'line'
+        name: "播放量",
+        type: "line",
+        smooth: true,
+        symbol: "circle",
+        symbolSize: 5,
+        showSymbol: false,
+        lineStyle: {
+          normal: {
+            color: "#0184d5",
+            width: 2
+          }
+        },
+        areaStyle: {
+          normal: {
+            color: new echarts.graphic.LinearGradient(0, 0, 0, 1,
+              [
+                {
+                  offset: 0,
+                  // 起始渐变色
+                  color: "rgba(1, 132, 213, 0.4)"
+                },
+                {
+                  offset: 0.8,
+                  // 结束渐变色
+                  color: "rgba(1, 132, 213, 0.1)"
+                }
+              ],
+              false
+            ),
+            shadowColor: "rgba(0, 0, 0, 0.1)"
+          }
+        },
+        itemStyle: {
+          normal: {
+            color: "#0184d5",
+            borderColor: "rgba(221, 220, 107, .1)",
+            borderWidth: 12
+          }
+        },
+        data: [
+          30, 40, 30, 40, 30, 40, 30, 60, 20, 40, 20, 40, 30,
+          40, 30, 40, 30, 40, 30, 60, 20, 40, 20, 40, 30, 60,
+          20, 40, 20, 40
+        ]
+      },
+      {
+        name: "转发量",
+        type: "line",
+        smooth: true,
+        // 拐点 及其样式
+        symbol: "circle",
+        symbolSize: 5,
+        // 鼠标指向才显示拐点
+        showSymbol: false,
+        lineStyle: {
+          normal: {
+            color: "#00d887",
+            width: 2
+          }
+        },
+        areaStyle: {
+          normal: {
+            color: new echarts.graphic.LinearGradient(0, 0, 0, 1,
+              [
+                {
+                  offset: 0,
+                  color: "rgba(0, 216, 135, 0.4)"
+                },
+                {
+                  offset: 0.8,
+                  color: "rgba(0, 216, 135, 0.1)"
+                }
+              ],
+              false
+            ),
+            shadowColor: "rgba(0, 0, 0, 0.1)"
+          }
+        },
+        itemStyle: {
+          normal: {
+            color: "#00d887",
+            borderColor: "rgba(221, 220, 107, .1)",
+            borderWidth: 12
+          }
+        },
+        data: [50, 30, 50, 60, 10, 50, 30, 50, 60, 40, 60, 40, 80, 30,
+          50, 60, 10, 50, 30, 70, 20, 50, 10, 40, 50, 30, 70, 20, 50,
+          10, 40
+        ]
       }
     ]
   };
-
-  option && myChart.setOption(option);
-
+  myChart.setOption(option);
+  window.addEventListener("resize", function () {
+    myChart.resize();
+  });
 })();
+// pie chart 
 (function () {
 
   var chartDom = document.querySelector('.pie .chart')
   var myChart = echarts.init(chartDom);
   var option;
   option = {
-
     tooltip: {
-
       trigger: "item",
-
       formatter: "{a} <br/>{b}: {c} ({d}%)",
-
       position: function (p) {
-
         //其中p为当前鼠标的位置
-
         return [p[0] + 10, p[1] - 10];
-
       }
 
     },
 
     legend: {
-
       top: "90%",
-
       itemWidth: 10,
-
       itemHeight: 10,
-
       data: ["0岁以下", "20-29岁", "30-39岁", "40-49岁", "50岁以上"],
-
       textStyle: {
-
         color: "rgba(255,255,255,.5)",
-
         fontSize: "12"
-
       }
-
     },
 
     series: [
-
       {
-
         name: "年龄分布",
-
         type: "pie",
-
+        //  圆在容器中的位置
         center: ["50%", "42%"],
-
+        //  饼形图 内圆和外圆的半径
         radius: ["40%", "60%"],
-
         color: [
-
-          "#065aab",
-
-          "#066eab",
-
-          "#0682ab",
-
-          "#0696ab",
-
-          "#06a0ab",
-
-          "#06b4ab",
-
-          "#06c8ab",
-
-          "#06dcab",
-
+          "#065aab", "#066eab", "#0682ab", "#0696ab",
+          "#06a0ab", "#06b4ab", "#06c8ab", "#06dcab",
           "#06f0ab"
-
         ],
-
+        // 图形上的文字
         label: { show: false },
-
+        // 连接标签文字和饼图的线
         labelLine: { show: false },
-
         data: [
-
           { value: 1, name: "0岁以下" },
-
           { value: 4, name: "20-29岁" },
-
           { value: 2, name: "30-39岁" },
-
           { value: 2, name: "40-49岁" },
-
           { value: 1, name: "50岁以上" }
-
         ]
-
       }
-
     ]
-
   };
-
-
-
-  // 使用刚指定的配置项和数据显示图表。
-
   myChart.setOption(option);
-
   window.addEventListener("resize", function () {
-
     myChart.resize();
-
   });
-
 })();
-
-// 学习进度柱状图模块
-
-
+// pie2 chart 
 (function () {
 
   var chartDom = document.querySelector('.pie2 .chart')
@@ -467,46 +569,63 @@
   var option;
   option = {
     legend: {
-      top: 'bottom'
-    },
-    toolbox: {
-      show: true,
-      feature: {
-        mark: { show: true },
-        dataView: { show: true, readOnly: false },
-        restore: { show: true },
-        saveAsImage: { show: true }
+      top: "90%",
+      itemWidth: 10,
+      itemHeight: 10,
+      textStyle: {
+        color: "rgba(255,255,255,.5)",
+        fontSize: "12"
       }
     },
+    tooltip: {
+      trigger: "item",
+      formatter: "{a} <br/>{b} : {c} ({d}%)"
+    },
+    // 注意颜色写的位置
+    color: [
+      "#006cff", "#60cda0", "#ed8884", "#ff9f7f",
+      "#0096ff", "#9fe6b8", "#32c5e9", "#1d9dff"
+    ],
     series: [
       {
-        name: 'Nightingale Chart',
-        type: 'pie',
-        radius: ['10%', '10%'],
-        center: ['50%', '50%'],
-        roseType: 'area',
-        itemStyle: {
-          borderRadius: 8
-        },
+        name: "点位统计",
+        type: "pie",
+        // 如果radius是百分比则必须加引号
+        radius: ["10%", "70%"],
+        center: ["50%", "42%"],
+        // 显示模式 radius半径模式 areas面积模式
+        roseType: "radius",
         data: [
-          { value: 40, name: 'rose 1' },
-          { value: 38, name: 'rose 2' },
-          { value: 32, name: 'rose 3' },
-          { value: 30, name: 'rose 4' },
-          { value: 28, name: 'rose 5' },
-          { value: 26, name: 'rose 6' },
-          { value: 22, name: 'rose 7' },
-          { value: 18, name: 'rose 8' }
-        ]
+          { value: 20, name: "云南" },
+          { value: 26, name: "北京" },
+          { value: 24, name: "山东" },
+          { value: 25, name: "河北" },
+          { value: 20, name: "江苏" },
+          { value: 25, name: "浙江" },
+          { value: 30, name: "深圳" },
+          { value: 42, name: "广东" }
+        ],
+        // 修饰饼形图文字相关的样式 label对象
+        label: {
+          fontSize: 10
+        },
+        // 修饰引导线样式
+        labelLine: {
+          // 连接到图形的线长度
+          length: 10,
+          // 连接到文字的线长度
+          length2: 10
+        }
       }
     ]
   };
-
-  option && myChart.setOption(option);
-
+  // 3. 配置项和数据给我们的实例化对象
+  myChart.setOption(option);
+  // 4. 当我们浏览器缩放的时候，图表也等比例缩放
+  window.addEventListener("resize", function () {
+    // 让我们的图表调用 resize这个方法
+    myChart.resize();
+  });
 })();
-
-
-
-
-
+// 模拟地图&&飞行路线
+// 引入china.js 中国地图
